843cad720dd3ab8d53c56282ac5317359c36b017
SeanMombo <Sean@mombourquette.com>
2020-09-10 11:29:35 -0400

    modal added

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, {useEffect, useState} from 'react';
+import Jobs from './Jobs';
+
+import './App.css';
+
+//project source: https://youtu.be/lauywdXKEXI?t=2141
+
+const JOB_API_URL = 'http://localhost:3001/jobs';
+
+async function fetchJobs(updateCb) {
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+
+  updateCb(json);
+  console.log(json)
+}
+
+function App() {
+
+  const [jobList, updateJobs] = useState([])
+  useEffect(() => {
+    fetchJobs(updateJobs);
+  }, []);
+  
+  return (
+    <div className="App">
+      <Jobs jobs={jobList}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Jobs.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+import Job from './Job';
+import JobModal from './jobModal'
+
+  
+
+export default function Jobs({jobs}) {
+
+    
+    // modal
+    const [open, setOpen] = React.useState(false);
+    const [selectedJob, selectJob] = React.useState({})
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
+    // pagination
+    const numJobs = jobs.length;
+    const numPages = Math.ceil(numJobs / 50);
+    const [activeStep, setActiveStep] = React.useState(0);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+    const jobsOnPage = jobs.slice(activeStep * 50, (activeStep * 50) + 50)
+
+
+
+    return (
+        <div className="jobs">
+            <JobModal open={open} job={selectedJob} handleClose={handleClose}/>
+            <Typography variant="h4" component="h1">
+                Entry Level Software Jobs
+            </Typography>
+            <Typography variant="h6" component="h1">
+                Found {numJobs} Jobs
+            </Typography>
+            {
+                jobsOnPage.map(
+                    (job, i) => <Job key ={i} job={job} onClick={() => {
+                        handleClickOpen();
+                        selectJob(job)
+                    }}/>
+                    
+                    )
+            }
+        
+            <div>
+                Page {activeStep + 1} of {numPages}
+            </div>
+
+            <MobileStepper
+                variant="dots"
+                steps={numPages}
+                position="static"
+                activeStep={activeStep}
+
+                nextButton={
+                    <Button size="small" onClick={handleNext} disabled={activeStep === numPages-1}>
+                        Next
+                        <KeyboardArrowRight />
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                     <KeyboardArrowLeft />
+                        Back
+                    </Button>
+                }
+            />
+        </div>
+    )
+}
